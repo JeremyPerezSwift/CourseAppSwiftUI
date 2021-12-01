3cda9e28287ee01b1fe9336910603970d2737dbf
JeremyPerezSwift <jeremyperez34000@gmail.com>
2021-12-01 16:21:11 +0100

    Initial Commit

---
+++ CourseAppSwiftUI/CourseAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseAppSwiftUIApp.swift
+//  CourseAppSwiftUI
+//
+//  Created by Jérémy Perez on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CourseAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
